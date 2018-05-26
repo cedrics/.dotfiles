185a2d164848eb20d98d16978e44a0bdfd2c1502
Cedric Sohrauer <sohrauer@gmail.com>
2018-05-26 16:36:03 +0200

    add zsh syntax highlighting

--- zsh/deps.zsh
+++ zsh/deps.zsh
@@ -6,6 +6,7 @@ export PATH=/usr/local/bin:$PATH
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 zplug "bhilburn/powerlevel9k", use:powerlevel9k.zsh-theme
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 if ! zplug check; then
   zplug install
