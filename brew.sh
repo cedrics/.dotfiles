c9eb21558d4ac1361c55dddcf2103832fbd654ec
Cedric Sohrauer <sohrauer@gmail.com>
2018-05-26 16:36:55 +0200

    Add some additional brew deps

--- brew.sh
+++ brew.sh
@@ -1,11 +1,17 @@
 HOMEBREW_NO_AUTO_UPDATE=1
+
 brew install fzf
 brew install goenv
 brew install hub
+brew install jq
 brew install neovim
+brew install ngrok
 brew install nodenv
 brew install postgres
 brew install rbenv
+brew install reattach-to-user-namespace
+brew install snappy
+brew install sshuttle
 brew install tmux
 
 brew cask install fork
