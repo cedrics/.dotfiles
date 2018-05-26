a7f59eb12ede8a9de8a35e1b3798266ab2937ee9
Cedric Sohrauer <sohrauer@gmail.com>
2018-05-26 12:55:07 +0200

    add brew file to install dependencies

---
+++ brew.sh
@@ -0,0 +1,12 @@
+HOMEBREW_NO_AUTO_UPDATE=1
+brew install fzf
+brew install goenv
+brew install hub
+brew install neovim
+brew install nodenv
+brew install postgres
+brew install rbenv
+brew install tmux
+
+brew cask install fork
+brew cask install visual-studio-code
