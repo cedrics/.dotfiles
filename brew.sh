9269f978b11887e7cee1a5e06571e606b91372ed
Cedric Sohrauer <sohrauer@gmail.com>
2018-06-06 16:25:58 +0200

    more dependencies

--- brew.sh
+++ brew.sh
@@ -1,18 +1,25 @@
 HOMEBREW_NO_AUTO_UPDATE=1
 
+brew install ag
 brew install fzf
 brew install goenv
+brew install httpie
 brew install hub
+brew install imagemagick
 brew install jq
+brew install libsass
+brew install mercurial
 brew install neovim
 brew install ngrok
 brew install nodenv
 brew install postgres
 brew install rbenv
 brew install reattach-to-user-namespace
+brew install redis
 brew install snappy
 brew install sshuttle
 brew install tmux
 
+brew cask install cyberduck
 brew cask install fork
 brew cask install visual-studio-code
